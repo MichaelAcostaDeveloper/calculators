bfb708860e59d8b005f5f7594561354d69fc83af
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-03-02 22:32:56 -0500

    Add a logic of the solution about array

--- logic/mensure_central_tendence.js
+++ logic/mensure_central_tendence.js
@@ -1,3 +1,4 @@
+
 //first step, we have to create an objects array
 signatures_all=[];
 
@@ -6,8 +7,25 @@ function registro(m,n,p){
     this.n=n;
     this.p=p;
 }
+var materia=document.getElementById("curso");
+var m=materia.value;
+
 //funcion que construye un arreglo de objetos
 function ingresar_registro(){
-    //var s=new registro(materia,nota,ponderacion);
-   // signatures_all.unshift(s)
+    //Pasa los valores de ingreso al arreglo
+    var materia=document.getElementById("curso").value;
+    var nota=Number(document.getElementById("nota").value);
+    var ponderacion=Number(document.getElementById("ponderacion").value);
+    if(materia===""||nota===""||ponderacion===""){
+        alert("Ingrese valores en los campos!")
+    }else{
+        var s=new registro(materia,nota,ponderacion);
+        signatures_all.unshift(s);
+        console.log(signatures_all);
+        alert("registro ingresado correctamente!");
+        document.getElementById('curso').value = '';
+        document.getElementById('nota').value = '';
+        document.getElementById('ponderacion').value = '';
+}
+
 }
