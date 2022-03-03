993e380d93b408bae3be181a2351038ce6625f85
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-03-02 20:28:10 -0500

    Design last on the styel sheet of the mensures of tendce centraL

---
+++ logic/mensure_central_tendence.js
@@ -0,0 +1,13 @@
+//first step, we have to create an objects array
+signatures_all=[];
+
+function registro(m,n,p){
+    this.m=m;
+    this.n=n;
+    this.p=p;
+}
+//funcion que construye un arreglo de objetos
+function ingresar_registro(){
+    //var s=new registro(materia,nota,ponderacion);
+   // signatures_all.unshift(s)
+}
