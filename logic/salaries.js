b9838fbdcd46685a6cb098eb27227399d44bfacc
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-03-23 15:21:27 -0500

    configuration of the last projects

--- logic/salaries.js
+++ logic/salaries.js
@@ -1,53 +1,46 @@
-const ecuador=[];
-ecuador.push({
-    name:"Juanita",
-    salary:1000,
-});
-ecuador.push({
-    name:"Juan",
-    salary:500,
-});
-ecuador.push({
-    name:"Pedro",
-    salary:1250,
-});
-ecuador.push({
-    name:"Pascual",
-    salary:120,
-});
-ecuador.push({
-    name:"Abdres",
-    salary:1630,
-});
-ecuador.push({
-    name:"Jose",
-    salary:1500,
-});
-ecuador.push({
-    name:"Omar",
-    salary:1000,
-});
-ecuador.push({
-    name:"Cami",
-    salary:1500,
-});
-ecuador.push({
-    name:"Jeremias",
-    salary:100,
-});
-ecuador.push({
-    name:"Stefany",
-    salary:1850,
-});
-ecuador.push({
-    name:"Perg",
-    salary:859,
-});
-ecuador.push({
-    name:"Daniel",
-    salary:250,
-});
+let elemento1=document.getElementById("edu");
+let elemento2=document.getElementById("vestido");
+let elemento3=document.getElementById("salud");
+let elemento4=document.getElementById("comida");
+let elemento5=document.getElementById("servicio");
+let elemento6=document.getElementById("transporte");
 
+let checkbox1=document.getElementById("clikear1");
+let checkbox2=document.getElementById("clikear2");
+let checkbox3=document.getElementById("clikear3");
+let checkbox4=document.getElementById("clikear4");
+let checkbox5=document.getElementById("clikear5");
+let checkbox6=document.getElementById("clikear6");
 
-
-
+function mostrar(){
+    if(checkbox1.checked){
+        elemento1.disabled=false
+    }else{
+        elemento1.disabled=true
+    }
+    if(checkbox2.checked){
+        elemento2.disabled=false
+    }else{
+        elemento2.disabled=true
+    }
+    if(checkbox3.checked){
+        elemento3.disabled=false
+    }else{
+        elemento3.disabled=true
+    }
+    if(checkbox4.checked){
+        elemento4.disabled=false
+    }else{
+        elemento4.disabled=true
+    }
+    if(checkbox5.checked){
+        elemento5.disabled=false
+    }else{
+        elemento5.disabled=true
+    }
+    if(checkbox6.checked){
+        elemento6.disabled=false
+    }else{
+        elemento6.disabled=true
+    }
+}
