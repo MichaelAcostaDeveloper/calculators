fc41a57bda2144460cf187a1d0ec785854247402
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-03-24 12:02:09 -0500

    Project Finished

--- logic/salaries.js
+++ logic/salaries.js
@@ -12,6 +12,9 @@ let checkbox4=document.getElementById("clikear4");
 let checkbox5=document.getElementById("clikear5");
 let checkbox6=document.getElementById("clikear6");
 
+let sueldo=document.getElementById("sueldo");
+let r=document.getElementById("resultados")
+
 function mostrar(){
     if(checkbox1.checked){
         elemento1.disabled=false
@@ -44,3 +47,23 @@ function mostrar(){
         elemento6.disabled=true
     }
 }
+
+function calculate_capacity(){
+
+    let sum=Number(elemento1.value)+
+            Number(elemento2.value)+
+            Number(elemento3.value)+
+            Number(elemento4.value)+
+            Number(elemento5.value)+
+            Number(elemento6.value);
+    let answer=Number(sueldo.value)-sum;
+    answer=answer*0.38;
+    console.log(sum);
+    console.log(answer);
+
+    if(answer > 0){
+        r.innerHTML="Your borrowing capacity is around: $"+answer;
+    }else{
+        r.innerHTML="STOP, you do not have enough money!";
+    }
+        }
